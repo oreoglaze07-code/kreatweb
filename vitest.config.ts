6192d7c8e81082fc19d6603dee3d73eaaa4c65a3
oreoglaze07-code <oreoglaze07@gmail.com>
2026-04-15 22:48:37 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/kreatweb/",   // ✅ ADD THIS LINE
+  base: "/kreatweb/",
   plugins: [react()],
   test: {
     environment: "jsdom",
